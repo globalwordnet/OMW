d98c92e6e243073d2a46d4a469ae7ecf470d152b
Francis Bond <bond@ieee.org>
2020-09-29 16:26:01 +0800

    fixed some wierdness for the lithuanian wordnet sharing a directory with slovakian

--- add-orphans.sh
+++ add-orphans.sh
@@ -10,14 +10,15 @@ RESDIR="$OMWROOT/omw/resources"
 
 declare -A lngs=(\
 		 ["arb"]="arb" \
-			["slk"]="sk" \
-		  	["isl"]="is" \
-		  	["als"]="als" \
-		  	["arb"]="arb" \
-		  	["ell"]="el"	\
-		  	["fra"]="fr"	\
-		  	["heb"]="he"	\
-		  	["hrv"]="hr"	\
+	 	 ["slk"]="sk" \
+	  	 ["isl"]="is" \
+	  	 ["als"]="als" \
+	  	 ["arb"]="arb" \
+	  	 ["ell"]="el"	\
+	  	 ["fra"]="fr"	\
+	  	 ["heb"]="he"	\
+	  	 ["hrv"]="hr"	\
+		 ["lit"]="lt"	\
                 )
 
 for lng in "${!lngs[@]}"
@@ -26,10 +27,16 @@ do
     ### fetch
     wget -nc -P ${RESDIR} http://compling.hss.ntu.edu.sg/omw/wns/${lng}.zip
     ### extract
-    unzip -j -d ${RESDIR} ${RESDIR}/${lng}.zip ${lng}/wn-data-${lng}.tab 
+    if [ $lng = 'lit' ]  ###
+    then
+	unzip -j -d ${RESDIR} ${RESDIR}/${lng}.zip slk/wn-data-${lng}.tab 
+    else
+	unzip -j -d ${RESDIR} ${RESDIR}/${lng}.zip ${lng}/wn-data-${lng}.tab 
+    fi	   
     ### convert
     python3 scripts/tsv2lmf.py ${lng}wn "${lngs[$lng]}" scripts/ili-map.tab ${RESDIR}/wn-data-${lng}.tab >  ${RESDIR}/${lng}wn.xml
     ### validate
     xmlstarlet validate -e --dtd omw/db/WN-LMF.dtd  ${RESDIR}/${lng}wn.xml
+    ### load
     
 done	    
